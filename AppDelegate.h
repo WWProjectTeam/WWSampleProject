8fa9917588dced40707327ee2c52b69b132a2cd7
oopww1992 <397294213@qq.com>
2015-09-09 15:54:16 +0800

    init

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WWSampleProject
+//
+//  Created by ww on 15/9/9.
+//  Copyright (c) 2015年 王维. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
